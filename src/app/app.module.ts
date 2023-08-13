8a5eb7be39bb76d5d5e6e9990a3f81cd8507ec1e
montemurro19 <montemurro.matheus@gmail.com>
2023-08-13 13:09:13 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardComponent } from './components/card/card.component';
+import { ButtonComponent } from './components/button/button.component';
+import { SelectorComponent } from './components/selector/selector.component';
+import { Button2Component } from './components/button2/button2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CardComponent,
+    ButtonComponent,
+    SelectorComponent,
+    Button2Component
   ],
   imports: [
     BrowserModule,
